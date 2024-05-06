7c40379290bf7ff590346ce729c906208cb13445
catavei <alexandru.catavei@gmail.com>
2024-05-06 12:08:10 +0300

    added docstrings

---
+++ train.py
@@ -0,0 +1,48 @@
+import numpy as np
+from config import *
+from utils import plot_data_over_episodes, pick_action, plot_q_table
+
+
+def train(env):
+    q_table = np.zeros(AREA_SIZE + (ACTION_SIZE,))
+    epsilon = EPSILON
+
+    rewards_per_episode = np.zeros(EPISODES)
+    steps_per_episode = np.zeros(EPISODES)
+
+    print("=== Training ===\n")
+
+    for episode in range(EPISODES):
+        state, _ = env.reset()
+        done = False
+        total_reward = 0
+        steps = 0
+        while not done:
+            if np.random.rand() <= epsilon:
+                action = np.random.randint(ACTION_SIZE)
+            else:
+                action = pick_action(state, q_table, ACTION_SIZE, AREA_SIZE, RANDOM_PICK_CHANCE)
+
+            next_state, reward, done, _, _ = env.step(action)
+            total_reward += reward
+
+            q_table[state + (action,)] += LEARNING_RATE * (
+                    reward + DISCOUNT_FACTOR * np.max(q_table[next_state]) - q_table[state + (action,)]
+            )
+
+            state = next_state
+            steps += 1
+
+        epsilon = max(epsilon * EPSILON_DECAY, EPSILON_MIN)
+        rewards_per_episode[episode] = total_reward
+        steps_per_episode[episode] = steps
+
+        if episode % 500 == 0:
+            print(f'Episode: {episode}, Epsilon: {epsilon:.2f}, Reward: {total_reward}, Steps: {steps}')
+
+    np.save('q_table.npy', q_table)
+
+    plot_data_over_episodes(rewards_per_episode, 'Smoothed Rewards Over Episodes', 'Average Reward')
+    plot_data_over_episodes(steps_per_episode, 'Smoothed Steps Per Episode', 'Average Steps per Episode')
+    plot_q_table(q_table)
+    return env
--- utils.py
+++ utils.py
@@ -2,7 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_data_over_episodes(data, title, y_label, window_size=100):
+def plot_data_over_episodes(data: np.ndarray, title: str, y_label: str, window_size: int = 500) -> None:
+    """
+        Description:
+            Plots smoothed data (rewards or steps) over episodes.
+
+        Args:
+            data (np.ndarray): The data array to plot, typically rewards or steps per episode.
+            title (str): The title of the plot.
+            y_label (str): The label for the y-axis.
+            window_size (int): The size of the window to use for smoothing the data with a moving average.
+    """
+
     smoothed_data = np.convolve(data, np.ones(window_size) / window_size, mode='valid')
     plt.figure(figsize=(12, 6))
     plt.plot(smoothed_data)
@@ -13,7 +24,16 @@ def plot_data_over_episodes(data, title, y_label, window_size=100):
     plt.show()
 
 
-def plot_q_table(q_table):
+def plot_q_table(q_table: np.ndarray) -> None:
+    """
+    Description:
+        Plots the Q-table as a heatmap with arrows indicating the best action per state.
+
+    Args:
+        q_table (np.ndarray): The Q-table to be plotted. It should be a 3D array where the first two
+                              dimensions correspond to the state space and the third dimension to action values.
+
+    """
     fig, ax = plt.subplots(figsize=(14, 14))
     max_q_values = np.max(q_table, axis=2)
     best_actions = np.argmax(q_table, axis=2)
@@ -21,7 +41,6 @@ def plot_q_table(q_table):
     c = ax.matshow(max_q_values, cmap='coolwarm')
     fig.colorbar(c, ax=ax)
 
-    # Define action vectors for arrow directions
     actions_dict = {0: (0, -1), 1: (0, 1), 2: (-1, 0), 3: (1, 0)}  # Assuming 0: up, 1: down, 2: left, 3: right
     for i in range(q_table.shape[0]):
         for j in range(q_table.shape[1]):
@@ -34,7 +53,24 @@ def plot_q_table(q_table):
     plt.show()
 
 
-def pick_action(state, q_table, action_size, area_size, random_pick_chance=0.1):
+def pick_action(state: tuple, q_table: np.ndarray, action_size: int, area_size: tuple, random_pick_chance: float = 0.1) -> int:
+    """
+     Args:
+        state (tuple): Current state of the agent, expressed as (y, x) coordinates.
+        q_table (numpy.ndarray): The Q-table containing accumulated Q-values for state-action pairs.
+        action_size (int): Total number of possible actions the agent can take.
+        area_size (tuple): Dimensions of the environment (height, width).
+        random_pick_chance (float): Probability of choosing a random action to facilitate exploration.
+
+    Description:
+        This function picks an action (up, down, left, right) for a given state from Q table.
+        It introduces a chance of choosing another action with a `random_pick_chance` probability to avoid
+        the case where two neighbouring states "point" to each other, introducing an infinite loop.
+
+    Returns:
+        int: The chosen action, which is either the best action according to the Q-table or a random valid action.
+    """
+
     y, x = state
     potential_actions = list(range(action_size))
 
@@ -50,7 +86,6 @@ def pick_action(state, q_table, action_size, area_size, random_pick_chance=0.1):
 
     best_action = max(potential_actions, key=lambda a: q_table[state][a])
 
-    # Introduce randomness to avoid deterministic loops
     if np.random.rand() < random_pick_chance:
         return np.random.choice(potential_actions)
     else:
